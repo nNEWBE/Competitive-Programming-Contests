d5076d84dc14428c69c23e3715aba5b575b0d0c5
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2025-10-22 10:45:39 +0600

    problem solved A - D

--- Contest_02/A.c
+++ Contest_02/A.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-int main(){
+int main()
+{
 
-    
-    
+    int n;
+    scanf("%d", &n);
+    if (n == 7)
+    {
+        printf("THALA\n");
+    }
+    else
+    {
+        printf("SADGE\n");
+    }
+
     return 0;
 }
---
+++ Contest_02/D.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n == 404)
+        printf("NOT FOUND");
+    else
+        printf("FOUND");
+    return 0;
+}
